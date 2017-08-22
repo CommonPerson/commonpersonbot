a3d5895d53f3efa170e1bf4fd48374f57fb0a24d
Serge Velikanov <s.velikanov@8bitgroup.com>
2017-08-22 19:29:30 +0300

    Initial Commit

---
+++ src/main/java/org/commonperson.bot/Bot.java
@@ -0,0 +1,9 @@
+package org.commonperson.bot;
+
+import org.springframework.boot.SpringApplication;
+
+public class Bot {
+    public static void main(String[] args) {
+        SpringApplication.run(Bot.class, args);
+    }
+}
